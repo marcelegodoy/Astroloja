37717ec5a591eda158e638e8c100bd8585cb3f4a
Marcele Godoy <marcelegodoy@gmail.com>
2020-02-26 15:43:12 -0300

    Seeds - Astroloja Project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+states_array = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"]
+
+states_array.each do |state_br|
+  State.create(name: state_br)
+end
+
+categories_array = ["Orixás", "Tarot de Marselha", "Búzios", "Trabalho de Magias", "Numerologia", "Orações", "Simpatias", "Magia Branca", "Cartomante", "Mapa Astral", "Baralho Cigano", "Quiromancia", "Florais", "Cromoterapia", "Magia Negra", "Reiki", "Umbanda", "Tarot Egípcio", "Paganismo", "Vidência", "Ciências Ocultas", "Wicca", "Sinastria", "Horóscopo Chinês", "Horóscopo", "Xamanismo", "Ocultismo", "Metafísica"]
+
+categories_array.each do |category_service|
+  Category.create(name: category_service)
+end
