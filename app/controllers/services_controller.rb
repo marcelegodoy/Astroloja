7d2493b54b9d13509524079041646f0e3f8aaed5
Andres Hernandez <aahrodriguez@gmail.com>
2020-02-26 18:41:28 -0300

    edit and update done

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -12,11 +12,25 @@ class ServicesController < ApplicationController
   end
 
   def edit
+    @service = Service.find(params[:id])
+
   end
 
   def update
+    @service = Service.find(params[:id])
+    if @service.update(service_params)
+      redirect_to service_path(@service)
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name)
   end
 end
