3c1058ac6aa7413c90a4d2a72daf7e0a531c441b
Marcele Godoy <marcelegodoy@gmail.com>
2020-02-28 16:56:56 -0300

    Resolving migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_173323) do
+ActiveRecord::Schema.define(version: 2020_02_28_141148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,8 +86,8 @@ ActiveRecord::Schema.define(version: 2020_02_28_173323) do
     t.string "description"
     t.string "city"
     t.bigint "state_id"
-    t.string "slug"
     t.string "username"
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["state_id"], name: "index_users_on_state_id"
