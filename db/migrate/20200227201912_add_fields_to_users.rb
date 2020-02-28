2994f205e0c8511c1e6fbd48bfef33fb20690da0
Andres Hernandez <aahrodriguez@gmail.com>
2020-02-28 14:55:07 -0300

    show user and own profile, done

--- db/migrate/20200227201912_add_fields_to_users.rb
+++ db/migrate/20200227201912_add_fields_to_users.rb
@@ -3,5 +3,6 @@ class AddFieldsToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :description, :string
     add_column :users, :city, :string
     add_reference :users, :state, foreign_key: true
+    add_column :users, :username, :string
   end
 end
